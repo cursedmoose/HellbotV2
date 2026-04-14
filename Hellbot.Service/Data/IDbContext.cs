49d3fd733ae80c364c47c422ee3c1e11b92d6287
CursedMoose <jim.duhala@gmail.com>
2026-04-13 18:00:43 -0700

    Add DbContext to hold Transaction Scope

---
+++ Hellbot.Service/Data/IDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+namespace Hellbot.Service.Data
+{
+    public interface IDbContext
+    {
+        IDbConnection Connection { get; }
+        IDbTransaction BeginTransaction();
+    }
+}
---
+++ Hellbot.Service/Data/SqliteDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace Hellbot.Service.Data
+{
+    public class SqliteDbContext(IDbConnectionFactory factory) : IDbContext
+    {
+        public IDbConnection Connection { get; } = factory.CreateConnection();
+
+        public IDbTransaction BeginTransaction()
+            => Connection.BeginTransaction();
+    }
+}
