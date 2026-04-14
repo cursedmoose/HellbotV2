0b5f052609b18db3e2927337f634519c5c987f40
CursedMoose <jim.duhala@gmail.com>
2026-04-13 18:04:18 -0700

    Add UserIdentities Table
    Holds their linked identities (twitch, youtube, etc)

---
+++ Hellbot.Service/Data/Migrations/M004_CreateUserIdentities.cs
@@ -0,0 +1,33 @@
+﻿using FluentMigrator;
+
+namespace Hellbot.Service.Data.Migrations
+{
+    [Migration(2026041202)]
+    public class M004_CreateUserIdentities : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("user_identities")
+                .WithColumn("id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("user_id").AsGuid()
+                .WithColumn("platform").AsString()
+                .WithColumn("platform_user_id").AsString()
+                .WithColumn("platform_user_name").AsString()
+                .WithColumn("linked_at").AsDateTime();
+
+            Create.UniqueConstraint("uq_platform_identity")
+                .OnTable("user_identities")
+                .Columns("platform", "platform_user_id");
+
+            Create.Index("idx_user_identities_user")
+                .OnTable("user_identities")
+                .OnColumn("user_id");
+        }
+
+        public override void Down()
+        {
+            Delete.Table("user_identities");
+
+        }
+    }
+}
---
+++ Hellbot.Service/Data/Tables/Users/UserIdentitiesTable.cs
@@ -0,0 +1,58 @@
+﻿using Dapper;
+using Hellbot.Core.Events;
+using Hellbot.Core.Users;
+using System.Data;
+
+namespace Hellbot.Service.Data.Tables.Users
+{
+    public class UserIdentitiesTable(IDbContext db)
+    {
+        public async Task Create(User user, UserIdentity identity, IDbTransaction? tx = null)
+        {
+            await db.Connection.ExecuteAsync(@"
+            INSERT INTO user_identities 
+            (user_id, platform, platform_user_id, platform_user_name, linked_at)
+            VALUES 
+            (@UserId, @Platform, @PlatformUserId, @PlatformUserName, @LinkedAt)
+            ", new
+            {
+                UserId = user.Id,
+                Platform = identity.Platform.ToString(),
+                PlatformUserId = identity.UserId,
+                PlatformUserName = identity.Username,
+                LinkedAt = DateTimeOffset.UtcNow,
+            },
+            transaction: tx);
+        }
+
+        public async Task<Guid?> GetUserId(PlatformSource platform, string userId)
+        {
+            return await db.Connection.QuerySingleOrDefaultAsync<Guid>(@"
+                SELECT user_id
+                FROM user_identities
+                WHERE platform = @Platform
+                  AND platform_user_id = @PlatformUserId
+            ",
+            new
+            {
+                Platform = platform.ToString(),
+                PlatformUserId = userId
+            });
+        }
+
+        public async Task<IReadOnlyList<UserIdentity>> Get(Guid userId)
+        {
+            return (await db.Connection.QueryAsync<UserIdentity>(@"
+                SELECT
+                    user_id AS UserId,
+                    platform AS Platform,
+                    platform_user_id AS PlatformUserId,
+                    platform_user_name AS PlatformUserName,
+                    linked_at AS LinkedAt
+                FROM user_identities
+                WHERE user_id = @UserId
+            ", new { UserId = userId }))
+            .AsList();
+        }
+    }
+}
