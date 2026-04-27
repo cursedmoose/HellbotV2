3981dd4b1962d8ccda3c7799653ec32d8e0829b0
CursedMoose <jim.duhala@gmail.com>
2026-04-26 20:48:06 -0700

    Add VoiceTranscriptionCompleted Event
    Sending the event back into the bus

---
+++ Hellbot.Core/Events/Audio/VoiceTranscriptionCompleted.cs
@@ -0,0 +1,10 @@
+﻿namespace Hellbot.Core.Events.Audio
+{
+    public record VoiceTranscriptionPayload
+    {
+        public required string Text;
+        public required DateTimeOffset Start;
+        public required DateTimeOffset End;
+    }
+    public record VoiceTranscriptionCompleted : HellbotEvent<VoiceTranscriptionPayload>;
+}
--- Hellbot.Service/EventBus/Handlers/Audio/SpeechToTextHandler.cs
+++ Hellbot.Service/EventBus/Handlers/Audio/SpeechToTextHandler.cs
@@ -1,11 +1,15 @@
-﻿using Hellbot.Core.Events.Audio;
+﻿using Hellbot.Core.Events;
+using Hellbot.Core.Events.Audio;
 using Hellbot.Service.Audio;
 using Hellbot.Service.Clients.Whisper;
 using NAudio.Wave;
 
 namespace Hellbot.Service.EventBus.Handlers.Audio
 {
-    public class SpeechToTextHandler(WhisperClient whisper, ILogger<SpeechToTextHandler> logger) : EventHandlerBase<VoiceSegmentCaptured>
+    public class SpeechToTextHandler(
+        WhisperClient whisper,
+        IEventBus bus,
+        ILogger<SpeechToTextHandler> logger) : EventHandlerBase<VoiceSegmentCaptured>
     {
         public async override Task Handle(VoiceSegmentCaptured evt)
         {
@@ -14,6 +18,17 @@ namespace Hellbot.Service.EventBus.Handlers.Audio
             var text = await whisper.TranscribeAsync(wavStream);
 
             logger.LogInformation("Text transcribed: {Text}", text);
+
+            await bus.Publish(new VoiceTranscriptionCompleted
+            {
+                Data = new()
+                {
+                    Start = evt.Data.Start,
+                    End = evt.Data.End,
+                    Text = text
+                },
+                Source = EventSource.Internal with { Channel = "SpeechToTextHandler" }
+            });
         }
     }
 }
