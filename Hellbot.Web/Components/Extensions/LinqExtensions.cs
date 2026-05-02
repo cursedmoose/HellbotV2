3f7ac2f8822d8b52870d03460448e772bd678b66
CursedMoose <jim.duhala@gmail.com>
2026-05-02 10:52:52 -0700

    Add Slightly better Event Filtering on UI

---
+++ Hellbot.Web/Components/Extensions/LinqExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Hellbot.UI.Components.Extensions
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<T> OrderByDynamic<T>(
+            this IEnumerable<T> source,
+            string property,
+            bool asc)
+        {
+            var prop = typeof(T).GetProperty(property);
+
+            return asc
+                ? source.OrderBy(x => prop!.GetValue(x, null))
+                : source.OrderByDescending(x => prop!.GetValue(x, null));
+        }
+    }
+}
---
+++ Hellbot.Web/Components/Model/EventViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace Hellbot.UI.Components.Model
+{
+    public class EventViewModel
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; } = "";
+        public DateTimeOffset Timestamp { get; set; }
+        public JsonElement Data { get; set; }
+    }
+}
