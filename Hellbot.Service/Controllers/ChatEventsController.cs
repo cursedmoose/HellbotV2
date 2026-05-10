118ca10ce95446421e5d260039cb0dc99440a5fa
CursedMoose <jim.duhala@gmail.com>
2026-05-09 20:45:59 -0700

    Add User overrides as query params to EventControllers

--- Hellbot.Service/Controllers/ChatEventsController.cs
+++ Hellbot.Service/Controllers/ChatEventsController.cs
@@ -8,26 +8,36 @@ namespace Hellbot.Service.Controllers
     public class ChatEventsController(IEventBus bus) : EventController(bus)
     {
         [HttpPost("chat/send")]
-        public Task<IActionResult> SendChatMessage(SendChatPayload evt)
-            => Publish(new SendChatMessage
-            {
-                Data = evt,
-                Source = EventSource.API
-            });
+        public async Task<IActionResult> SendChatMessage(
+            SendChatPayload evt,
+            [FromQuery] Guid? asHellbotUserId = null,
+            [FromQuery] string? asTwitchLogin = null)
+        {
+            var hellbotEvt = new SendChatMessage { Data = evt, Source = EventSource.API };
+            var rejection = SeedUserContext(hellbotEvt, asHellbotUserId, asTwitchLogin);
+            return rejection ?? await Publish(hellbotEvt);
+        }
+
         [HttpPost("chat/receive")]
-        public Task<IActionResult> ReceiveChatMessage(ChatReceivedPayload evt)
-            => Publish(new ChatMessageReceived
-            {
-                Data = evt,
-                Source = EventSource.API
-            });
+        public async Task<IActionResult> ReceiveChatMessage(
+            ChatReceivedPayload evt,
+            [FromQuery] Guid? asHellbotUserId = null,
+            [FromQuery] string? asTwitchLogin = null)
+        {
+            var hellbotEvt = new ChatMessageReceived { Data = evt, Source = EventSource.API };
+            var rejection = SeedUserContext(hellbotEvt, asHellbotUserId, asTwitchLogin);
+            return rejection ?? await Publish(hellbotEvt);
+        }
 
         [HttpPost("tts")]
-        public Task<IActionResult> TtsMessage(TtsRequestPayload evt)
-            => Publish(new TtsRequested
-            {
-                Data = evt,
-                Source = EventSource.API
-            });
+        public async Task<IActionResult> TtsMessage(
+            TtsRequestPayload evt,
+            [FromQuery] Guid? asHellbotUserId = null,
+            [FromQuery] string? asTwitchLogin = null)
+        {
+            var hellbotEvt = new TtsRequested { Data = evt, Source = EventSource.API };
+            var rejection = SeedUserContext(hellbotEvt, asHellbotUserId, asTwitchLogin);
+            return rejection ?? await Publish(hellbotEvt);
+        }
     }
 }
--- Hellbot.Service/Controllers/EventController.cs
+++ Hellbot.Service/Controllers/EventController.cs
@@ -1,4 +1,5 @@
 ﻿using Hellbot.Core.Events;
+using Hellbot.Core.Users;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hellbot.Service.Controllers
@@ -7,6 +8,29 @@ namespace Hellbot.Service.Controllers
     [ApiController]
     public abstract class EventController(IEventBus bus) : ControllerBase
     {
+        protected IActionResult? SeedUserContext(
+            IHellbotEvent evt,
+            Guid? asHellbotUserId,
+            string? asTwitchLogin)
+        {
+            var twitchLogin = string.IsNullOrWhiteSpace(asTwitchLogin)
+                ? null
+                : asTwitchLogin.Trim();
+            var hasHellbotUserId = asHellbotUserId.HasValue;
+            var hasTwitchLogin = twitchLogin is not null;
+
+            if (hasHellbotUserId && hasTwitchLogin)
+                return BadRequest("Specify only one of asHellbotUserId or asTwitchLogin.");
+
+            if (hasHellbotUserId)
+                evt.Context = EventContext.From(new UserLocator.HellbotUser(asHellbotUserId!.Value));
+            else if (hasTwitchLogin)
+                evt.Context = EventContext.From(
+                    new UserLocator.PlatformUsername(PlatformSource.Twitch, twitchLogin!));
+
+            return null;
+        }
+
         protected async Task<IActionResult> Publish(IHellbotEvent evt)
         {
             await bus.Publish(evt);
